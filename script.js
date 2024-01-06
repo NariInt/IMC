f57c2e21ecea5ed78c54028bd64d6bc3e8e827e1
Iran Sousa <125208621+NariInt@users.noreply.github.com>
2024-01-06 14:37:03 -0300

    js

--- script.js
+++ script.js
@@ -16,7 +16,18 @@ form.addEventListener('submit', function(event){
 
     if(bmi < 18.5){
         description = 'Cuidado, você está abaixo do peso'
+    } else if(bmi >= 18.5 && bmi <= 25){
+        description = 'Você está no peso ideal';
+    } else if(bmi > 25  && bmi <= 30){
+        description = 'Cuidado, você está com sobrepeso!';
+    } else if(bmi > 30  && bmi <= 35){
+        description = 'Cuidado! você está com obesidade moderada!';
+    } else if(bmi > 35  && bmi <= 40){
+        description = 'Cuidado! você está com obesidade severa!';
+    } else{
+        description = 'Cuidado! você está com obesidade morbida!'
     }
+    value.textContent = bmi.replace('.',',');
     document.getElementById('description').textContent = description;
 
 });
